4e1f86ad4c1617e066673bab07dd7ed6c2e9040d
Brahmantya Wardhana <bwardhana@hawk.iit.edu>
2024-11-15 20:05:47 -0600

    added template for admin and staff page frontend

--- admin.php
+++ admin.php
@@ -11,3 +11,15 @@ if (!isset($_SESSION['user_id']) || $_SESSION['role'] != 'admin') {
 // Admin-specific content here
 echo "Welcome Admin, " . $_SESSION['username'] . "!";
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Home Page</title>
+        <link rel="stylesheet" href="css/style.css">
+    </head>
+    <body>
+        <p>Admin interface</p>
+    </body>
--- staff.php
+++ staff.php
@@ -11,3 +11,15 @@ if (!isset($_SESSION['user_id']) || $_SESSION['role'] != 'staff') {
 // Staff-specific content here
 echo "Welcome Staff, " . $_SESSION['username'] . "!";
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Home Page</title>
+        <link rel="stylesheet" href="css/style.css">
+    </head>
+    <body>
+        <p>Staff interface</p>
+    </body>
