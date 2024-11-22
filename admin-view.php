1cc692c9ef939dd3b8f6fdf88df37e4d9483e2d1
Brahmantya Wardhana <bwardhana@hawk.iit.edu>
2024-11-22 16:15:21 -0600

    fixed incorrect row

--- admin-view.php
+++ admin-view.php
@@ -58,7 +58,7 @@ $doctor = mysqli_query($con,$query);
                                         <td><?php echo $row['lastName']; ?></td>
                                         <td><?php echo $row['specialization']; ?></td>
                                         <td><?php echo $row['status']; ?></td>
-                                        <td><?php echo $row['doctorID']; ?></td>
+                                        <td><?php echo $row['email']; ?></td>
                                     </tr>
                                     <?php } ?>
                                 </tbody>
