7591d4568c0591740266eede1f9cedd987f25b23
Brahmantya Wardhana <bwardhana@hawk.iit.edu>
2024-11-22 20:36:06 -0600

    seperate table css

--- admin-view.php
+++ admin-view.php
@@ -29,7 +29,7 @@ $doctor = mysqli_query($con,$query);
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>Home Page</title>
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet">
-        <link rel="stylesheet" href="css/style.css">
+        <link rel="stylesheet" href="css/table-view.css">
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js"></script>
     </head>
     <body>
