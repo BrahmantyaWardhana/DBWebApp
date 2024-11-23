b2b519be617e8ed9399d04c78c420182832810c5
Brahmantya Wardhana <bwardhana@hawk.iit.edu>
2024-11-22 20:28:48 -0600

    trying overflow table

--- README.md
+++ README.md
@@ -3,5 +3,5 @@ web apllication for itmd-422
 
 Notes:
 - php code for Local DB connection only
-- need to improve logout
 - not built to check for hashed password (tested with plaintext password only)
+- figure out how to make each table display scrollable in a container not the page
--- admin-view.php
+++ admin-view.php
@@ -28,6 +28,7 @@ $doctor = mysqli_query($con,$query);
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>Home Page</title>
+        <link rel="stylesheet" href="css/style.css">
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet">
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js"></script>
     </head>
@@ -36,6 +37,7 @@ $doctor = mysqli_query($con,$query);
             <div class="row">
                 <div class="col">
                     <div class="card mt-5">
+                        <!-- Start of Doctor view -->
                         <div class="card-reader">
                             <h2 class="display-6 text-center">Display Doctors Table Test</h2>
                             <a class="btn btn-primary" href="admin-doctorAdd.php" role="button">New Entry</a>
@@ -72,6 +74,7 @@ $doctor = mysqli_query($con,$query);
                                 </tbody>
                             </table>
                         </div>
+                        <!-- End of Doctor view -->
                     </div>
                 </div>
             </div>
