9b8a85397950b00cb5dc4ee3044dc91e55a0654b
Brahmantya Wardhana <bwardhana@hawk.iit.edu>
2024-11-30 19:13:19 -0600

    login

--- admin-scheduleAdd.php
+++ admin-scheduleAdd.php
@@ -61,6 +61,10 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet">
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js"></script>
     </head>
+    <body>
+        <form action="logout.php" method="POST">
+            <button type="submit">Logout</button>
+        </form>
         <div class="container my-5">
             <h2 class="display-6 text-center">Add Payments Table</h2>
 
@@ -117,7 +121,4 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
                 </div>
             </form>
         </div>
-        <form action="logout.php" method="POST">
-            <button type="submit">Logout</button>
-        </form>
     </body>
